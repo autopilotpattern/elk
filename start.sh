aa2a589cc2b5a6e203a702469315ff6fabda702c
Tim Gross <tim@0x74696d.com>
2015-12-01 11:27:06 -0500

    Initialize Kibana indexes remotely

--- start.sh
+++ start.sh
@@ -5,6 +5,9 @@ set -e -o pipefail
 export COMPOSE_PROJECT_NAME=elk
 source common.sh
 
+# suppress docker-compose warnings
+export DOCKER_CLIENT_TIMEOUT=
+
 docker-compose up -d \
                elasticsearch \
                elasticsearch_master \
@@ -21,20 +24,51 @@ poll-for-page "http://$(getIpPort elasticsearch_master 9200)/_cluster/health?pre
               'Waiting for Elasticsearch...' \
               'Opening cluster status page.'
 
+
+# ---------------------------------------------------
+# Kibana configuration
+
+echo 'Initializing Kibana indexes...'
+
+# write an initial log entry so that we can use dynamic field mapping.
+while :
+do
+    echo $(printf '%s localhost kibana: initializing index' "$(date '+%b %d %H:%M:%S')") | \
+        nc $(echo $(getIpPort logstash 514) | awk '{split($0,a,":"); print a[1],a[2]}') && break
+    sleep 1
+    echo -ne .
+done
+echo
+
+# Kibana currently requires manual intervention to create index patterns:
+# https://github.com/elastic/kibana/issues/5199
+# We'll work around this via writing directly to ES
+# ref https://github.com/elastic/kibana/issues/3709#issuecomment-140453042
+
+curl -XPUT -s --fail \
+     -d '{"title" : "logstash-*",  "timeFieldName": "@timestamp"}' \
+     "http://$(getIpPort elasticsearch_master 9200)/.kibana/index-pattern/logstash-*"
+
+curl -XPOST -s --fail \
+     -d $(printf '{"doc":{"doc":{"buildNum":%s,"defaultIndex":"%s"},"defaultIndex":"%s"}}' \
+                 9369 "logstash-*" "logstash-*") \
+     "http://$(getIpPort elasticsearch_master 9200)/.kibana/config/4.3.0/_update"
+
 # poll Kibana for liveness and then open the page
-poll-for-page "http://$(getIpPort kibana 5601)/app/kibana" \
+poll-for-page "http://$(getIpPort kibana 5601)/app/kibana#discover" \
               'Waiting for Kibana to register as healthy...' \
               'Opening Kibana console.'
 
-# write an initial log entry so that we can use dynamic field mapping
-echo 'Initializing Kibana indexes...'
-nc $(echo $(getIpPort logstash 514) | awk '{split($0,a,":"); print a[1],a[2]}') <<EOF
-echo $(printf '%s localhost kibana: initializing index' "$(date '+%b %d %H:%M:%S')") |
-EOF
 
+# ---------------------------------------------------
+# Test client
 
 echo 'Starting Nginx log source...' && \
      LOGSTASH=$(getPrivateIpPort logstash 514) \
      CONTAINERBUDDY="$(cat ./nginx/containerbuddy.json)" \
-     NGINX_CONF="$(cat ./nginx/nginx.ctmpl)" \
+     NGINX_CONF="$(cat ./nginx/nginx.conf)" \
      docker-compose up -d nginx
+
+poll-for-page "http://$(getIpPort nginx 80)" \
+              'Waiting for Nginx to register as healthy...' \
+              'Opening web page.'
